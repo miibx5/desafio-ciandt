66a115b1ed5edfead83a35012116263081954422
Éder <miibx5@gmail.com>
2020-03-02 19:40:54 -0300

    Project Created

---
+++ src/main/java/br/com/edersystems/desafiociandt/DesafioCiandtApplication.java
@@ -0,0 +1,15 @@
+package br.com.edersystems.desafiociandt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCiandtApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DesafioCiandtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/edersystems/desafiociandt/DesafioCiandtApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.edersystems.desafiociandt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCiandtApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
