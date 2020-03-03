60fd2ac0f6f3732c4c02d29a0caf0dc47905553b
Éder <miibx5@gmail.com>
2020-03-03 20:54:53 -0300

    Endpoint de entrega funcionando

---
+++ readme.md
@@ -0,0 +1,9 @@
+# README
+
+# Em primeiro lugar clonar o projeto em: (https://github.com/miibx5/desafio-ciandt.git)
+# Com o projeto clonado criar usuário e database utilizando postgresql seguindo configurações no arquivo application.properties.
+# Entrar na pasta do projeto e rodar o comando: mvn clean install para baixar as dependências do projeto.
+# Parar executar o projeto basta executar o seguinte comando: mvn spring-boot:run.
+# Os testes não foram implementados, caso tivessem sido implementados o comando para executá-los seria: mvn clean test
+# Utilizar um aplicativo com a função de testar api's.
+# Também não foram feitas validações das requests, não por desconhecimento, somente por não ter sido requisitado.  
---
+++ src/test/java/br/com/edersystems/desafiociandt/model/service/mapa/MapaServiceTest.java
@@ -0,0 +1,30 @@
+package br.com.edersystems.desafiociandt.model.service.mapa;/*
+Project .....................: desafio-ciandt
+Creation Date ...............: 03/03/20 20:29:45
+Developer....................: ederluciano
+Copyright....................: 2020
+Codification.................: UTF-8
+...........................................................
+ Éder L. Costa - © Copyright 2020 - All Rights Reserved
+...........................................................
+*/
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MapaServiceTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @Test
+    public void getAllMapas()
+    {
+    }
+}
